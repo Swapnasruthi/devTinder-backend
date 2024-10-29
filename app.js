4fa2a39c5ad8fea6417d3d009d97fe2b8f30f4de
Swapnasruthi <swapnasruthi2005@gmail.com>
2024-10-29 14:27:29 +0530

    creates-auth-middlewares

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const {adminAuth} = require("./middlewares/auth");
 const app = express();
 
-app.use((req,res)=>{
-    res.send("hello");
+
+app.use("/admin",adminAuth)
+app.use("/admin/login",(req,res)=>{
+    res.send("user login");
 });
+
+app.use("/admin/delete",(req,res)=>{
+    res.send("user deleted");
+})
+
+
 app.listen(3000);
